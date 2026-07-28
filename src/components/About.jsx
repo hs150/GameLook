8bfcb06c0b1f4a2ee755ccc159b3a3c51f034304
harsh <hs150harsh@gmail.com>
2026-07-29 00:50:47 +0530

    About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,35 @@
+import { useGSAP } from '@gsap/react'
 import React from 'react'
+import gsap from 'gsap'
+
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
+
 
 const About = () => {
+
+
+    useGSAP(()=>{
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#clip',
+                start: 'center center',
+                end: '+=800 center',
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: 'true',
+            }
+        })
+
+        clipAnimation.to('.mask-clip-path',{
+            width: '100vw',
+            height: '100vh',
+            borderRadius: 0
+        })
+
+    })
+
   return (
     <div id="about" className="min-h-screen w-screen">
         <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
@@ -22,7 +51,15 @@ const About = () => {
             </div>
         </div>
 
-        
+        <div className="h-dvh w-screen" id="clip">
+            <div className = "mask-clip-path about-image">
+                <img
+                src ="img/about.webp"
+                alt="Background"
+                className=" absolute left-0 top-0 size-full object-cover"
+                />
+            </div>
+        </div>
    
     </div>
   )
