3424b6d8a7de49661f75f9b3a7d6b9a05b12dbc9
harsh <hs150harsh@gmail.com>
2026-08-07 22:24:35 +0530

    km

---
+++ src/components/Features.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <section className="bg-black pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32">
+            <p className="font-circular-web text-lg text-blue-50">Into the MetaGames</p>
+        </div>
+        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+            Immerse yourself in a rich and ever-expanding universe of games, where each title offers unique experiences and challenges. Our platform is designed to bring together a diverse range of games, from action-packed adventures to strategic puzzles, ensuring that there's something for every type of gamer. Join our community and explore the endless possibilities that await in the world of MetaGames.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Features
